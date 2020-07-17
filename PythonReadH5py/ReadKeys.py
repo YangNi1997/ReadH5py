6927bec280bf42e48f94ada698ed992f1755e4f8
Yang Ni <970085090@qq.com>
2020-07-17 15:36:45 +0800

    code is improved(function readKeys and try-catch)

--- PythonReadH5py/ReadKeys.py
+++ PythonReadH5py/ReadKeys.py
@@ -2,20 +2,11 @@
 import numpy as np
 import h5py
 
-content=[]
-hf=h5py.File('/home/ni/ReadData/c_H_in_CH2.h5','r')
-for key in hf.keys():
-    content.append(hf[key].name)
-    subf=hf.get(key)
-    print(hf[key].name)
-    for key in subf.keys():
-        content.append(subf[key].name)
-        #print(subf[key].name)
-        #print(subf[key].items())
-        subff=hf.get(subf[key].name)
-        for key in subff.keys():
-            content.append(subff[key].name)
-           #print(subff[key].name)
-            #print(subff[key].items())
-for var in content:
-   print(var)
+def readKeys(content, file):
+    try:
+        for key in file.keys():
+            content.append(file[key].name)
+            subfile=file.get(file[key].name)
+            readKeys(content,subfile)
+    except AttributeError as e:
+        pass
