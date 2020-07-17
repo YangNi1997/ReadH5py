fcdd5fb71dc3d58351c7df6c179f27ce0c6ab153
Yang Ni <970085090@qq.com>
2020-07-17 13:33:36 +0800

    no KeyError

--- PythonReadH5py/ReadFile.py
+++ PythonReadH5py/ReadFile.py
@@ -10,13 +10,12 @@ for key in hf.keys():
     print(hf[key].name)
     for key in subf.keys():
         content.append(subf[key].name)
-        print(subf[key].name)
+        #print(subf[key].name)
         #print(subf[key].items())
         subff=hf.get(subf[key].name)
         for key in subff.keys():
             content.append(subff[key].name)
-            print(subff[key].name)
+           #print(subff[key].name)
             #print(subff[key].items())
-#for var in content:
-#    print(var)
-#print(content)
+for var in content:
+   print(var)
