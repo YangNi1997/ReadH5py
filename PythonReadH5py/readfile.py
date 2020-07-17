df1991e9857d9fb55c20ffde76fe28873bccd788
Yang Ni <970085090@qq.com>
2020-07-17 09:33:03 +0800

    KeyError: 'Unable to open object (bad object header version number)'

---
+++ PythonReadH5py/readfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import numpy as np
+import h5py
+
+content=[]
+hf=h5py.File('/home/ni/ReadData/c_H_in_CH2.h5','r')
+for key in hf.keys():
+    content.append(hf[key].name)
+    subf=hf.get(key)
+    print(hf[key].name)
+    for key in subf.keys():
+        content.append(subf[key].name)
+        print(subf[key].name)
+        #print(subf[key].items())
+        subff=hf.get(subf[key].name)
+        for key in subff.keys():
+            content.append(subff[key].name)
+            print(subff[key].name)
+            #print(subff[key].items())
+#for var in content:
+#    print(var)
+#print(content)
